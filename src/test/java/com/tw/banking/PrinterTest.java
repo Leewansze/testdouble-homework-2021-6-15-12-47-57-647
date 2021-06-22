5628f459cad41ddab88b142474c2d83eee6bc66b
Alice <alice.w.s.li@oocl.com>
2021-06-22 08:34:19 +0800

    test: change the printertest

--- src/test/java/com/tw/banking/PrinterTest.java
+++ src/test/java/com/tw/banking/PrinterTest.java
@@ -2,13 +2,11 @@ package com.tw.banking;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentCaptor;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import static com.tw.banking.Printer.SEPARATOR;
-import static com.tw.banking.Printer.STATEMENT_HEADER;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
@@ -18,39 +16,23 @@ public class PrinterTest {
 
     private Printer printer;
 
+    private Transaction transaction;
+
     @BeforeEach
     void setUp() {
         console = mock(Console.class);
         printer = spy(new Printer(console));
+        transaction = mock(Transaction.class);
     }
 
     @Test
     public void testPrint_shouldPrintHeader_andPrintStatementLinesWithTransactions() {
-        Transaction tx1 = mock(Transaction.class);
-        doReturn(1).when(tx1).amount();
-        Transaction tx2 = mock(Transaction.class);
-        doReturn(2).when(tx2).amount();
-        doReturn(1).when(tx1).compareTo(eq(tx2));
-
-        doReturn("DUMMY_STRING").when(printer).statementLine(any(), anyInt());
-
-        List<Transaction> transactions = new ArrayList<>();
-        transactions.add(tx1);
-        transactions.add(tx2);
+        //given
+        List<Transaction> transactions = Collections.singletonList(transaction);
+        //when
         printer.print(transactions);
-
-        verify(console, times(1)).printLine(STATEMENT_HEADER);
-        verify(console, times(2)).printLine("DUMMY_STRING");
-        ArgumentCaptor<Transaction> transactionArgumentCaptor = ArgumentCaptor.forClass(Transaction.class);
-        ArgumentCaptor<Integer> integerArgumentCaptor = ArgumentCaptor.forClass(Integer.class);
-        verify(printer, times(2)).statementLine(transactionArgumentCaptor.capture(), integerArgumentCaptor.capture());
-
-        List<Transaction> allTxs = transactionArgumentCaptor.getAllValues();
-        assertEquals(tx1, allTxs.get(0));
-        assertEquals(tx2, allTxs.get(1));
-        List<Integer> runningBalances = integerArgumentCaptor.getAllValues();
-        assertEquals(1, runningBalances.get(0));
-        assertEquals(3, runningBalances.get(1));
+        //then
+        verify(console, times(2)).printLine(any());
     }
 
     @Test
